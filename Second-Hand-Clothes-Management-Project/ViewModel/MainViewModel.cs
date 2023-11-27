17d6e59ffec2ec8018710697edabb6b97b92ecbd
bichngoc55 <22520957@gm.uit.edu.vn>
2023-11-27 16:26:09 +0700

    new commit

---
+++ Second-Hand-Clothes-Management-Project/ViewModel/MainViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Second_Hand_Clothes_Management_Project.ViewModel
+{
+    public  class MainViewModel : BaseViewModel
+    {
+    }
+}
