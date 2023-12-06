044699ee2fe183ea470f7b3bf06c74d515fc09ab
Tánh Trần Tuệ <22521314@ms.uit.edu.vn>
2023-12-06 19:43:36 +0700

    sanphamviewmodel

---
+++ Second-Hand-Clothes-Management-Project/ViewModel/SanPhamViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Second_Hand_Clothes_Management_Project.View;
+
+namespace Second_Hand_Clothes_Management_Project.ViewModel
+{
+    public class SanPhamViewModel : BaseViewModel
+    {
+    }
+}
