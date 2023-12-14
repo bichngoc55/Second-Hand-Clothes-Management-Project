aebe905aa1e76b72bc2019d3c41fe74cd03dd499
Tánh Trần Tuệ <22521314@ms.uit.edu.vn>
2023-12-14 23:44:01 +0700

    them caidat

--- Second-Hand-Clothes-Management-Project/Model/Const.cs
+++ Second-Hand-Clothes-Management-Project/Model/Const.cs
@@ -12,5 +12,7 @@ namespace Second_Hand_Clothes_Management_Project.Model
         public static string TenDangNhap { get; set; }
         public static bool Admin { get; set; }
         public static NGUOIDUNG ND { get; set; }
+
+        public static string _localLink = System.Reflection.Assembly.GetExecutingAssembly().Location.Remove(System.Reflection.Assembly.GetExecutingAssembly().Location.IndexOf(@"bin\Debug"));
     }
 }
